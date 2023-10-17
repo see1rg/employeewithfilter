1c16ec178297b77d7895c209a001a72b7161c757
see1rg <see1rg@mail.ru>
2023-10-17 13:01:10 +0300

    start

---
+++ src/main/java/com/see1rg/listofcars/ListofcarsApplication.java
@@ -0,0 +1,13 @@
+package com.see1rg.listofcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListofcarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListofcarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/see1rg/listofcars/ListofcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.see1rg.listofcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListofcarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
